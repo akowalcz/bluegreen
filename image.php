3254dc81270deb8f80292dff67a1f22295dd9031
akowalcz <akowalcz@redhat.com>
2020-04-01 14:42:32 +0200

    Update image.php

--- image.php
+++ image.php
@@ -8,7 +8,7 @@ $deployment = getenv("COLOR");
 if ( $deployment == 'blue') {
   $color = imagecolorallocate($im, 0, 255, 0);
 } elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 0, 0, 255);
+  $color = imagecolorallocate($im, 0, 255, 255);
 } else {
   $color = imagecolorallocate($im, 255, 0, 0);
 }
