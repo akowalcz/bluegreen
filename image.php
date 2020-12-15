8f1061e682fff5e63acd085d972637f374eb1d0b
akowalcz <akowalcz@redhat.com>
2020-12-15 13:22:27 +0100

    right colors

--- image.php
+++ image.php
@@ -2,19 +2,19 @@
 // Create a 55x30 image
 $im = imagecreatetruecolor(200, 200);
 $white = imagecolorallocate($im, 255, 255, 255);
-
+// Line reserved to trigger the webhook in the lab. 
 $deployment = getenv("COLOR");
 
 if ( $deployment == 'blue') {
-  $color = imagecolorallocate($im, 0, 255, 255);
+  $color = imagecolorallocate($im, 0, 0, 255);
 } elseif ($deployment == 'green')  {
-  $color = imagecolorallocate($im, 255, 0, 255);
+  $color = imagecolorallocate($im, 0, 255, 0);
 } else {
-  $color = imagecolorallocate($im, 255, 255, 255);
+  $color = imagecolorallocate($im, 0, 0, 255);
 }
 
 // Draw a filled rectangle
-imagefilledrectangle($im, 0, 0, 0, 0, $color);
+imagefilledrectangle($im, 0, 0, 199, 199, $color);
 
 // Save the image
 header('Content-Type: image/png');
